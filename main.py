16ae2fd24c5b744b26073301e0c3a866edf84caa
crissdot <crissdotdev@gmail.com>
2024-07-31 00:13:51 +0000

    add tiny improvements

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client #{client_id} left the chat")
